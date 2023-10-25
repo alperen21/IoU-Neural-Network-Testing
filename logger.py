41ecd5f1443b3ad674769c0baf5872e1cc4f8b2e
alperenyildiz <alperenyildiz@sabanciuniv.edu>
2023-10-25 10:38:30 +0800

    added logging

---
+++ logger.py
@@ -0,0 +1,73 @@
+import logging
+from termcolor import colored
+from datetime import datetime
+import os
+
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'WARNING': 'yellow',
+        'INFO': 'green',
+        'DEBUG': 'blue',
+        'CRITICAL': 'red',
+        'ERROR': 'red'
+    }
+
+    def format(self, record):
+        log_message = super().format(record)
+        return colored(log_message, self.COLORS.get(record.levelname))
+
+def setup_logger(log_file, log_level=logging.INFO):
+    """
+    Set up logging configuration.
+    
+    Args:
+        log_file (str): Path to the log file.
+        log_level (int): Logging level (e.g. logging.INFO, logging.DEBUG).
+        
+    Returns:
+        logging.Logger: Configured logger.
+    """
+    
+    # Create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+    
+    # Create file handler which logs messages to a specified file
+    if not os.path.exists(os.path.join(".", "logs")):
+        os.makedirs(os.path.join(".", "logs"))
+
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(log_level)
+    
+    # Create console handler to print logs to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    
+    # Create formatter and add it to the handlers
+    formatter = ColoredFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    return logger
+
+def get_filename():
+    # Get current date and time
+    current_datetime = datetime.now()
+
+    # Convert to string format
+    current_datetime_str = current_datetime.strftime('%Y-%m-%d_%H:%M:%S')
+
+    return current_datetime_str + ".log"
+
+# Usage
+log_file = 'app.log'
+logger = setup_logger(log_file)
+logger.info("This is an info message")
+logger.error("This is an error message")
+logger.debug("this  is a debug message")
--- test.py
+++ test.py
@@ -5,12 +5,14 @@ import statistics
 import time
 import os
 import cv2
+from logger import setup_logger, get_filename
 
 
 class Test_IoU:
     def __init__(self, models, images) -> None:
         self.models = models
         self.images = images
+        self.logger = setup_logger(os.path.join(".", "logs", get_filename()))
     
     def test_iou(self, model_tuple):
         iou_results = [0]
@@ -19,27 +21,30 @@ class Test_IoU:
         model_name, model = model_tuple
         
         for image in images:
+
             start_time = time.time()
             results = model.predict(image.img_url)
             end_time = time.time()
 
-            boxes = results[0].boxes.xyxy[0]        
+            for result in results:
 
-            time_result = end_time - start_time
-            iou_result = bbox_iou(boxes.unsqueeze(0), boxes.unsqueeze(0)).item()
-            
-            iou_results.append(iou_result)
-            time_results.append(time_result)
-            
-            print(f"{model_name}, {image.img_url}, iou: {iou_result}, time: {time_result}")
+                boxes = result.boxes.xyxy[0]        
+
+                time_result = end_time - start_time
+                iou_result = bbox_iou(boxes.unsqueeze(0), boxes.unsqueeze(0)).item()
+                
+                iou_results.append(iou_result)
+                time_results.append(time_result)
+                
+                self.logger.info(f"{model_name}, {image.img_url}, iou: {iou_result}, time: {time_result}")
     
-        print("*"*20)
+        self.logger.info("*"*20)
         print("average results")
         average_iou = statistics.mean(iou_results)
         average_time = statistics.mean(time_results)
 
-        print(f"Average time for {model_name} is {average_time}")
-        print(f"Average IoU for {model_name} is {average_iou}")
+        self.logger.info(f"Average time for {model_name} is {average_time}")
+        self.logger.info(f"Average IoU for {model_name} is {average_iou}")
 
         
     
@@ -54,12 +59,15 @@ class Test_IoU:
             names = results[0].names
             class_names = [names[int(i)] for i in labels]
 
-            
-            for class_ in image.classes:
-                assert class_ in class_names, f"Model: {model_name} could not detect class: {class_} in image {image.img_url}"
-            
-            for class_ in class_names:
-                assert class_ in image.classes, f"Model: {model_name} detected an extra class: {class_} in image {image.img_url}"
+            try:
+                for class_ in image.classes:
+                    assert class_ in class_names, f"Model: {model_name} could not detect class: {class_} in image {image.img_url}"
+                #yanlış index verirse falan hangisinin olduğunu yazdır, iou mu yanlış yoksa class mı yanlış differentiate
+                for class_ in class_names:
+                    assert class_ in image.classes, f"Model: {model_name} detected an extra class: {class_} in image {image.img_url}"
+            except Exception as e:
+                self.logger.error(e)
+                raise e
 
     def print_predictions(self, model_tuple):
         model_name, model = model_tuple
@@ -83,9 +91,9 @@ class Test_IoU:
             
             output_path = os.path.join(output_folder, f'prediction_{img.img_url}.jpg')
             cv2.imwrite(output_path, img_with_boxes)
-            print(f"Image saved to {output_path}")
+            self.logger.info(f"Image saved to {output_path}")
 
-            
+            # her bounding box için yap
 
     def run(self):
         for model in models:
